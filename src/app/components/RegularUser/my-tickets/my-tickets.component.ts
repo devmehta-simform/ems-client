b97075496a4ea0908d376e262b52ca40483c36e4
dev mehta <dev.mehta@simformsolutions.com>
2025-03-31 16:05:55 +0530

    chore: create some basic components

---
+++ src/app/components/RegularUser/my-tickets/my-tickets.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-tickets',
+  imports: [],
+  templateUrl: './my-tickets.component.html',
+  styleUrl: './my-tickets.component.css',
+})
+export class MyTicketsComponent {}
---
+++ src/app/components/RegularUser/upcoming-events/upcoming-events.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upcoming-events',
+  imports: [],
+  templateUrl: './upcoming-events.component.html',
+  styleUrl: './upcoming-events.component.css',
+})
+export class UpcomingEventsComponent {}
