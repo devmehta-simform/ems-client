e346f4b56451cfc6335e22d5c04786d966b2dd53
dev mehta <dev.mehta@simformsolutions.com>
2025-04-08 13:57:46 +0530

    feat: add loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoaderService } from './services/loader.service';
+import { AsyncPipe } from '@angular/common';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, LoaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  loader$;
+  constructor(private loaderService: LoaderService) {
+    this.loader$ = this.loaderService.getLoader$();
+  }
   title = 'client';
 }
