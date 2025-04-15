1cf26c6e8ea2f1e8a77216e90cc45fd9abd98a96
dev mehta <dev.mehta@simformsolutions.com>
2025-04-15 19:11:45 +0530

    feat: add carousel buttons

--- src/app/components/event/event-list/event-list.component.ts
+++ src/app/components/event/event-list/event-list.component.ts
@@ -11,11 +11,29 @@ import { Event } from '../../../../types';
 })
 export class EventListComponent implements OnInit {
   eventList!: Event[];
+  currentIndex = 1;
   constructor(private eventService: EventService) {}
 
   ngOnInit() {
     this.eventService.getEvents().subscribe(data => {
       this.eventList = [...data, ...data];
     });
+  }
+
+  prevSlide() {
+    this.currentIndex = this.currentIndex > 0 ? this.currentIndex - 1 : this.eventList.length - 1;
+    this.scrollToCurrentSlide();
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.eventList.length;
+    this.scrollToCurrentSlide();
+  }
+
+  scrollToCurrentSlide() {
+    const element = document.getElementById('slide' + this.currentIndex);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
+    }
   }
 }
