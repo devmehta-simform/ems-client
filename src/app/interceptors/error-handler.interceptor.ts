4b15fb54a3d6336046dad2aaeedc3622ab15b94f
devmehta-simform <dev.mehta@simformsolutions.com>
2025-04-28 12:53:56 +0530

    feat: navigate to auth if res has status 401

--- src/app/interceptors/error-handler.interceptor.ts
+++ src/app/interceptors/error-handler.interceptor.ts
@@ -2,12 +2,17 @@ import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { AlertService } from '../services/alert.service';
+import { Router } from '@angular/router';
 
 export const errorHandlerInterceptor: HttpInterceptorFn = (req, next) => {
   const alertService = inject(AlertService);
+  const router = inject(Router);
   return next(req).pipe(
     catchError((res: HttpErrorResponse) => {
       console.error(res);
+      if (res.status === 401) {
+        router.navigate(['auth']);
+      }
       alertService.show(res.error.error.message);
       return throwError(() => res.error);
     })
