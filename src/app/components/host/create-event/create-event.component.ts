4d74360bba1cbd63bc9d478a40d51e50e22a9090
devmehta-simform <dev.mehta@simformsolutions.com>
2025-04-30 14:06:47 +0530

    refactor: refactor create-event component for refactoring and including appropriate inputs

--- src/app/components/host/create-event/create-event.component.ts
+++ src/app/components/host/create-event/create-event.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country, State, City, type ICountry, type IState, type ICity } from 'country-state-city';
 
 @Component({
   selector: 'app-create-event',
@@ -6,4 +7,46 @@ import { Component } from '@angular/core';
   templateUrl: './create-event.component.html',
   styleUrl: './create-event.component.css',
 })
-export class CreateEventComponent {}
+export class CreateEventComponent {
+  countries: ICountry[];
+  country!: ICountry;
+  states!: IState[];
+  cities!: ICity[];
+  state!: IState;
+  today: string = new Date().toISOString().split('T')[0];
+  constructor() {
+    this.countries = Country.getAllCountries();
+  }
+
+  getStates() {
+    return State.getStatesOfCountry(this.country.isoCode);
+  }
+
+  getCities() {
+    return City.getCitiesOfState(this.country.isoCode, this.state.isoCode);
+  }
+
+  changeCountry(event: Event) {
+    const ele = event.target;
+    if (ele instanceof HTMLSelectElement) {
+      // console.log(ele.value);
+      const foundCountry = Country.getCountryByCode(ele.value);
+      if (foundCountry !== undefined) {
+        this.country = foundCountry;
+        this.states = this.getStates();
+        this.cities = [];
+      }
+    }
+  }
+
+  changeState(event: Event) {
+    const ele = event.target;
+    if (ele instanceof HTMLSelectElement) {
+      const foundState = State.getStateByCodeAndCountry(ele.value, this.country.isoCode);
+      if (foundState !== undefined) {
+        this.state = foundState;
+        this.cities = this.getCities();
+      }
+    }
+  }
+}
