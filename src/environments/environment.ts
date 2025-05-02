acfb79e0551f3e74194465c53d781ac946beb7cd
devmehta-simform <dev.mehta@simformsolutions.com>
2025-05-02 10:51:38 +0530

    chore: add env

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  API_BASE_URL: 'http://localhost:3000',
+  CLOUDINARY_CLOUD_NAME: 'dlucxwxap',
+  CLOUDINARY_API_SECRET: 'XTuQw0-ZlDTmv1fvgGXNWbaoYxI',
+  CLOUDINARY_API_KEY: '679186333863751',
+};
