691750dd3a9ca05b9d77179c0ad4e172602fb696
devmehta-simform <dev.mehta@simformsolutions.com>
2025-05-02 10:43:19 +0530

    fix: profile section by adding username and avatar

--- src/app/components/guest/navbar/navbar.component.ts
+++ src/app/components/guest/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { UserService } from '../../../services/user.service';
+import { UserLoginSchema } from '../../../../response-types';
+import { DataStoreService } from '../../../services/data-store.service';
 
 @Component({
   selector: 'app-guest-navbar',
@@ -9,10 +11,23 @@ import { UserService } from '../../../services/user.service';
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  username: string | null;
+  avatar: string | null;
+
   constructor(
     private userService: UserService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private dataStoreService: DataStoreService
+  ) {
+    const user = UserLoginSchema.safeParse(this.dataStoreService.getData('user'));
+    if (user.success) {
+      this.username = user.data.name || null;
+      this.avatar = user.data.avatar || null;
+    } else {
+      this.username = null;
+      this.avatar = null;
+    }
+  }
   handleLogout() {
     this.userService.logout();
     this.router.navigate(['/auth']);
--- src/app/components/host/sidebar/sidebar.component.ts
+++ src/app/components/host/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { UserService } from '../../../services/user.service';
+import { UserLoginSchema } from '../../../../response-types';
+import { DataStoreService } from '../../../services/data-store.service';
 
 @Component({
   selector: 'app-host-sidebar',
@@ -9,4 +12,25 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class SidebarComponent {
   isVisible = false;
+  username: string | null;
+  avatar: string | null;
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private dataStoreService: DataStoreService
+  ) {
+    const user = UserLoginSchema.safeParse(this.dataStoreService.getData('user'));
+    if (user.success) {
+      this.username = user.data.name || null;
+      this.avatar = user.data.avatar || null;
+    } else {
+      this.username = null;
+      this.avatar = null;
+    }
+  }
+
+  logoutUser() {
+    this.userService.logout();
+    this.router.navigate(['/auth']);
+  }
 }
