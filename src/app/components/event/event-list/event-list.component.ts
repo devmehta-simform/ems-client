a7257634b23ddc20d3d62270ea4d0ab4b86a542a
devmehta-simform <dev.mehta@simformsolutions.com>
2025-07-14 06:34:56 +0530

    style: properly display event search results

--- src/app/components/event/event-list/event-list.component.ts
+++ src/app/components/event/event-list/event-list.component.ts
@@ -4,14 +4,17 @@ import { EventComponent } from '../event/event.component';
 import { EventSchema } from '../../../../response-types';
 import { z } from 'zod';
 import { map, Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CurrencyPipe, DatePipe } from '@angular/common';
+import { CloudinaryImagePipe } from '../../../pipes/cloudinary-image.pipe';
+import { RouterLink } from '@angular/router';
+import { ImgFallbackDirective } from '../../../directives/img-fallback.directive';
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const EventsSchema = z.array(EventSchema);
 
 @Component({
   selector: 'app-event-list',
-  imports: [EventComponent, AsyncPipe],
+  imports: [EventComponent, AsyncPipe, DatePipe, CurrencyPipe, RouterLink, CloudinaryImagePipe, ImgFallbackDirective],
   templateUrl: './event-list.component.html',
   styleUrl: './event-list.component.css',
 })
@@ -19,9 +22,15 @@ export class EventListComponent implements OnInit {
   eventList$!: Observable<z.infer<typeof EventsSchema>>;
   private eventList!: z.infer<typeof EventsSchema>;
   currentIndex = 0;
-  constructor(private eventService: EventService) {}
+  public isSearchMode = false;
+  private searchQuery = '';
+  constructor(public eventService: EventService) {}
 
   ngOnInit() {
+    this.eventService['search$'].subscribe((query: string) => {
+      this.searchQuery = query;
+      this.isSearchMode = !!query;
+    });
     this.eventList$ = this.eventService
       .getSearch$()
       .pipe(map(data => (this.eventList = data.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()))));
--- src/app/components/guest/explore/explore.component.ts
+++ src/app/components/guest/explore/explore.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { EventListComponent } from '../../event/event-list/event-list.component';
 import { TrendingEventsComponent } from '../trending-events/trending-events.component';
 
@@ -8,4 +8,16 @@ import { TrendingEventsComponent } from '../trending-events/trending-events.comp
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.css',
 })
-export class ExploreComponent {}
+export class ExploreComponent implements AfterViewInit {
+  @ViewChild(EventListComponent) eventListComponent!: EventListComponent;
+  isSearchMode = false;
+
+  ngAfterViewInit() {
+    if (this.eventListComponent) {
+      // Use a MutationObserver or polling since isSearchMode is set asynchronously
+      setInterval(() => {
+        this.isSearchMode = this.eventListComponent['isSearchMode'];
+      }, 200);
+    }
+  }
+}
